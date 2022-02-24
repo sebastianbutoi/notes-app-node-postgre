a85f95a310a20b70f27df4fbf9f0e13f77b24f73
Sebastian Butoi <butoi.sebastian@yahoo.com>
2022-02-24 00:06:15 +0000

    Express serving static file

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 import express from "express";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static(__dirname + "/public"));
+
 app.listen(PORT, () => {
-  console.log("Listening on port: ", PORT);
+  console.log("Listening on port:", PORT);
 });
